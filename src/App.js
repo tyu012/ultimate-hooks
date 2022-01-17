57936ff8d9e90a6ee602c5536226384cb0720cfd
Tim Yu <yuyu.tim@gmail.com>
2022-01-16 21:56:56 -0800

    implemented useResource hook

--- src/App.js
+++ src/App.js
@@ -20,10 +20,14 @@ const useField = (type) => {
 const useResource = (baseUrl) => {
   const [resources, setResources] = useState([])
 
-  // ...
+  useEffect(() => {
+    axios.get(baseUrl)
+      .then(response => setResources(response.data))
+  }, [baseUrl])
 
   const create = (resource) => {
-    // ...
+    axios.post(baseUrl, resource)
+      .then(response => setResources([...resources, response.data]))
   }
 
   const service = {
